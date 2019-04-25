0f7e925a09ecaf5a6c98d611ba9b4d9a0d3cda07
Andrew Millar <duckman.andrew@gmail.com>
2019-04-25 12:08:11 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+(function(){
+  console.log("version 0.95");
+
+  // Placing code inside self executing function to encapsulate code and not create globals
+
+  window.onload = function() {init()}
+  // window.addEventListener("load", init());
+
+  const init = function() {
+    // Initiate the script
+  }
+
+})();
